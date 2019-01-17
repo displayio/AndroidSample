9c5f27e66d6dfbea572736cae0147b15817a5a32
Ariel Malka <ariel@chronotext.org>
2019-01-17 16:37:07 +0200

    Starting with fragments

--- app/src/main/java/io/display/androidsample/MainActivity.java
+++ app/src/main/java/io/display/androidsample/MainActivity.java
@@ -2,6 +2,7 @@ package io.display.androidsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, new MainFragment())
+                    .commitNow();
+        }
     }
 }
