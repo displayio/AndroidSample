b164ff79d43d258f31ccac266e43a8ed8fa1a274
Ariel Malka <ariel@chronotext.org>
2019-01-20 17:24:07 +0200

    Basic behavior in place in InterstitialFragment but ...
    ... Problem when coming back to MainFragment: nothing displayed because "no adapter attached".
    Solution may be to switch to activities instead of fragments

--- app/src/main/java/io/display/androidsample/InterstitialFragment.java
+++ app/src/main/java/io/display/androidsample/InterstitialFragment.java
@@ -12,13 +12,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import io.display.androidsample.utils.FragmentHelper;
+import io.display.sdk.AdProvider;
+import io.display.sdk.AdRequest;
+import io.display.sdk.Controller;
+import io.display.sdk.Placement;
+import io.display.sdk.ads.Ad;
+import io.display.sdk.exceptions.DioSdkException;
+import io.display.sdk.listeners.AdLoadListener;
+import io.display.sdk.listeners.AdRequestListener;
 
 public class InterstitialFragment extends Fragment {
 
+    static String TAG = "XXX"; // XXX
+
     Button loadButton;
     Button showButton;
+
+    String placementId;
+    Ad loadedAd;
 
     @Override
     public void onStart() {
@@ -38,7 +52,7 @@ public class InterstitialFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Bundle args = getArguments();
-        String placementId = args.getString("placementId");
+        placementId = args.getString("placementId");
 
         View contentView = inflater.inflate(R.layout.fragment_interstitial, container, false);
         loadButton = contentView.findViewById(R.id.button_load_interstitial);
@@ -47,18 +61,68 @@ public class InterstitialFragment extends Fragment {
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("XXX", "LOAD");
+                loadAd();
             }
         });
 
         showButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("XXX", "SHOW");
+                showAd();
             }
         });
 
         return contentView;
+    }
+
+    private void loadAd() {
+        loadButton.setEnabled(false);
+
+        Placement placement;
+        try {
+            placement = Controller.getInstance().getPlacement(placementId);
+        } catch (DioSdkException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+            return;
+        }
+
+        AdRequest adRequest = placement.newAdRequest();
+        adRequest.setAdRequestListener(new AdRequestListener() {
+            @Override
+            public void onAdReceived(AdProvider adProvider) {
+
+                adProvider.setAdLoadListener(new AdLoadListener() {
+                    @Override
+                    public void onLoaded(Ad ad) {
+                        loadedAd = ad;
+                        showButton.setEnabled(true);
+                    }
+
+                    @Override
+                    public void onFailedToLoad() {
+                        Toast.makeText(getContext(), "Ad for placement " + placementId + " failed to load", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                try {
+                    adProvider.loadAd();
+                } catch(DioSdkException e) {
+                    Log.e(TAG, e.getLocalizedMessage());
+                }
+            }
+
+            @Override
+            public void onNoAds() {
+                Toast.makeText(getContext(), "No Ads placement " + placementId, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        adRequest.requestAd();
+    }
+
+    private void showAd() {
+        showButton.setEnabled(false);
+        loadedAd.showAd(getContext());
     }
 
     @Override
