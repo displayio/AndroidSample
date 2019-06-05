fad2bf1f2f22517eae48b034ca67b99645a339eb
Bitlejuce <Bitlejuce@gmail.com>
2019-06-05 09:43:46 +0300

    Update dependesies to brandio.ads SDK 3.0.0

--- app/src/main/java/com/brandio/androidsample/ShowListWithBannerActivity.java
+++ app/src/main/java/com/brandio/androidsample/ShowListWithBannerActivity.java
@@ -1,13 +1,12 @@
-package com.brandio.androidsample;
+package io.display.androidsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-
-
 
 public class ShowListWithBannerActivity extends AppCompatActivity {
 
